8ff2c4cb9734134e13d1b286801bd6e23f176dc4
Mitchell Henke <mitchell@mitchellhenke.com>
2026-03-01 08:44:39 -0600

    vacuum reachable roads

--- brokenspoke_analyzer/scripts/sql/connectivity/reachable_roads_high_stress_cleanup.sql
+++ brokenspoke_analyzer/scripts/sql/connectivity/reachable_roads_high_stress_cleanup.sql
@@ -2,4 +2,4 @@ CREATE UNIQUE INDEX IF NOT EXISTS idx_neighborhood_rchblrdshistrss_b
 ON generated.neighborhood_reachable_roads_high_stress (
     base_road, target_road
 );
--- VACUUM ANALYZE generated.neighborhood_reachable_roads_high_stress;
+VACUUM ANALYZE generated.neighborhood_reachable_roads_high_stress;
