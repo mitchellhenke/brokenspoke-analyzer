7bd7181b4ce274c02a61e91118912e28cad3237d
Mitchell Henke <mitchell@mitchellhenke.com>
2026-06-23 14:20:55 -0500

    fix network link build speed

--- brokenspoke_analyzer/scripts/sql/connectivity/build_network_links.sql
+++ brokenspoke_analyzer/scripts/sql/connectivity/build_network_links.sql
@@ -6,213 +6,79 @@
 ---------------
 -- add links --
 ---------------
--- two-way to two-way
--- slow
+-- A net_link represents travelling INTO an intersection along one road
+-- (the "source") and back OUT along another (the "target"). Rather than
+-- enumerating every one_way/end combination as a separate 5-way join with an
+-- `int_id IN (from, to)` disjunction (which forces a per-intersection bitmap
+-- re-scan of the ways table), build the source/target incidences once and
+-- self-join them on int_id.
+--
+-- A road touches an intersection at intersection_from or intersection_to.
+-- It can be a SOURCE (driven toward the intersection) at:
+--   intersection_to   if two-way (one_way IS NULL) or 'ft'
+--   intersection_from if two-way (one_way IS NULL) or 'tf'
+-- It can be a TARGET (driven away from the intersection) at:
+--   intersection_from if two-way (one_way IS NULL) or 'ft'
+--   intersection_to   if two-way (one_way IS NULL) or 'tf'
+-- UNION (not UNION ALL) collapses the duplicate row a two-way self-loop
+-- (intersection_from = intersection_to) would otherwise produce.
 INSERT INTO received.neighborhood_ways_net_link (
     int_id, source_vert, target_vert, geom
 )
-SELECT
-    ints.int_id,
-    vert1.vert_id,
-    vert2.vert_id, -- noqa: AL08
-    ST_Makeline(vert1.geom, vert2.geom) -- noqa: AL03
-FROM received.neighborhood_ways_intersections AS ints,
-    received.neighborhood_ways_net_vert AS vert1,
-    received.neighborhood_ways AS roads1,
-    received.neighborhood_ways_net_vert AS vert2,
-    received.neighborhood_ways AS roads2
-WHERE
-    vert1.road_id = roads1.road_id
-    AND vert2.road_id = roads2.road_id
-    AND ints.int_id IN (roads1.intersection_from, roads1.intersection_to)
-    AND ints.int_id IN (roads2.intersection_from, roads2.intersection_to)
-    AND roads1.one_way IS NULL
-    AND roads2.one_way IS NULL
-    AND roads1.road_id != roads2.road_id;
+WITH src AS (
+    SELECT
+        roads.intersection_to AS int_id,
+        vert.vert_id,
+        vert.geom,
+        roads.road_id
+    FROM received.neighborhood_ways AS roads
+    INNER JOIN received.neighborhood_ways_net_vert AS vert
+        USING (road_id)
+    WHERE roads.one_way IS NULL OR roads.one_way = 'ft'
+    UNION
+    SELECT
+        roads.intersection_from AS int_id,
+        vert.vert_id,
+        vert.geom,
+        roads.road_id
+    FROM received.neighborhood_ways AS roads
+    INNER JOIN received.neighborhood_ways_net_vert AS vert
+        USING (road_id)
+    WHERE roads.one_way IS NULL OR roads.one_way = 'tf'
+),
 
--- two-way to from-to
-INSERT INTO received.neighborhood_ways_net_link (
-    int_id, source_vert, target_vert, geom
+tgt AS (
+    SELECT
+        roads.intersection_from AS int_id,
+        vert.vert_id,
+        vert.geom,
+        roads.road_id
+    FROM received.neighborhood_ways AS roads
+    INNER JOIN received.neighborhood_ways_net_vert AS vert
+        USING (road_id)
+    WHERE roads.one_way IS NULL OR roads.one_way = 'ft'
+    UNION
+    SELECT
+        roads.intersection_to AS int_id,
+        vert.vert_id,
+        vert.geom,
+        roads.road_id
+    FROM received.neighborhood_ways AS roads
+    INNER JOIN received.neighborhood_ways_net_vert AS vert
+        USING (road_id)
+    WHERE roads.one_way IS NULL OR roads.one_way = 'tf'
 )
-SELECT
-    ints.int_id,
-    vert1.vert_id,
-    vert2.vert_id, -- noqa: AL08
-    ST_Makeline(vert1.geom, vert2.geom) -- noqa: AL03
-FROM received.neighborhood_ways_intersections AS ints,
-    received.neighborhood_ways_net_vert AS vert1,
-    received.neighborhood_ways AS roads1,
-    received.neighborhood_ways_net_vert AS vert2,
-    received.neighborhood_ways AS roads2
-WHERE
-    vert1.road_id = roads1.road_id
-    AND vert2.road_id = roads2.road_id
-    AND ints.int_id IN (roads1.intersection_from, roads1.intersection_to)
-    AND ints.int_id = roads2.intersection_from
-    AND roads1.one_way IS NULL
-    AND roads2.one_way = 'ft'
-    AND roads1.road_id != roads2.road_id;
 
--- two-way to to-from
-INSERT INTO received.neighborhood_ways_net_link (
-    int_id, source_vert, target_vert, geom
-)
 SELECT
-    ints.int_id,
-    vert1.vert_id,
-    vert2.vert_id, -- noqa: AL08
-    ST_Makeline(vert1.geom, vert2.geom) -- noqa: AL03
-FROM received.neighborhood_ways_intersections AS ints,
-    received.neighborhood_ways_net_vert AS vert1,
-    received.neighborhood_ways AS roads1,
-    received.neighborhood_ways_net_vert AS vert2,
-    received.neighborhood_ways AS roads2
-WHERE
-    vert1.road_id = roads1.road_id
-    AND vert2.road_id = roads2.road_id
-    AND ints.int_id IN (roads1.intersection_from, roads1.intersection_to)
-    AND ints.int_id = roads2.intersection_to
-    AND roads1.one_way IS NULL
-    AND roads2.one_way = 'tf'
-    AND roads1.road_id != roads2.road_id;
-
--- from-to to two-way
-INSERT INTO received.neighborhood_ways_net_link (
-    int_id, source_vert, target_vert, geom
-)
-SELECT
-    ints.int_id,
-    vert1.vert_id,
-    vert2.vert_id, -- noqa: AL08
-    ST_Makeline(vert1.geom, vert2.geom) -- noqa: AL03
-FROM received.neighborhood_ways_intersections AS ints,
-    received.neighborhood_ways_net_vert AS vert1,
-    received.neighborhood_ways AS roads1,
-    received.neighborhood_ways_net_vert AS vert2,
-    received.neighborhood_ways AS roads2
-WHERE
-    vert1.road_id = roads1.road_id
-    AND vert2.road_id = roads2.road_id
-    AND ints.int_id = roads1.intersection_to
-    AND ints.int_id IN (roads2.intersection_from, roads2.intersection_to)
-    AND roads1.one_way = 'ft'
-    AND roads2.one_way IS NULL
-    AND roads1.road_id != roads2.road_id;
-
--- from-to to from-to
-INSERT INTO received.neighborhood_ways_net_link (
-    int_id, source_vert, target_vert, geom
-)
-SELECT
-    ints.int_id,
-    vert1.vert_id,
-    vert2.vert_id, -- noqa: AL08
-    ST_Makeline(vert1.geom, vert2.geom) -- noqa: AL03
-FROM received.neighborhood_ways_intersections AS ints,
-    received.neighborhood_ways_net_vert AS vert1,
-    received.neighborhood_ways AS roads1,
-    received.neighborhood_ways_net_vert AS vert2,
-    received.neighborhood_ways AS roads2
-WHERE
-    vert1.road_id = roads1.road_id
-    AND vert2.road_id = roads2.road_id
-    AND ints.int_id = roads1.intersection_to
-    AND ints.int_id = roads2.intersection_from
-    AND roads1.one_way = 'ft'
-    AND roads2.one_way = 'ft'
-    AND roads1.road_id != roads2.road_id;
-
--- from-to to to-from
-INSERT INTO received.neighborhood_ways_net_link (
-    int_id, source_vert, target_vert, geom
-)
-SELECT
-    ints.int_id,
-    vert1.vert_id,
-    vert2.vert_id, -- noqa: AL08
-    ST_Makeline(vert1.geom, vert2.geom) -- noqa: AL03
-FROM received.neighborhood_ways_intersections AS ints,
-    received.neighborhood_ways_net_vert AS vert1,
-    received.neighborhood_ways AS roads1,
-    received.neighborhood_ways_net_vert AS vert2,
-    received.neighborhood_ways AS roads2
-WHERE
-    vert1.road_id = roads1.road_id
-    AND vert2.road_id = roads2.road_id
-    AND ints.int_id = roads1.intersection_to
-    AND ints.int_id = roads2.intersection_to
-    AND roads1.one_way = 'ft'
-    AND roads2.one_way = 'tf'
-    AND roads1.road_id != roads2.road_id;
-
--- to-from to two-way
-INSERT INTO received.neighborhood_ways_net_link (
-    int_id, source_vert, target_vert, geom
-)
-SELECT
-    ints.int_id,
-    vert1.vert_id,
-    vert2.vert_id, -- noqa: AL08
-    ST_Makeline(vert1.geom, vert2.geom) -- noqa: AL03
-FROM received.neighborhood_ways_intersections AS ints,
-    received.neighborhood_ways_net_vert AS vert1,
-    received.neighborhood_ways AS roads1,
-    received.neighborhood_ways_net_vert AS vert2,
-    received.neighborhood_ways AS roads2
-WHERE
-    vert1.road_id = roads1.road_id
-    AND vert2.road_id = roads2.road_id
-    AND ints.int_id = roads1.intersection_from
-    AND ints.int_id IN (roads2.intersection_from, roads2.intersection_to)
-    AND roads1.one_way = 'tf'
-    AND roads2.one_way IS NULL
-    AND roads1.road_id != roads2.road_id;
-
--- to-from to to-from
-INSERT INTO received.neighborhood_ways_net_link (
-    int_id, source_vert, target_vert, geom
-)
-SELECT
-    ints.int_id,
-    vert1.vert_id,
-    vert2.vert_id, -- noqa: AL08
-    ST_Makeline(vert1.geom, vert2.geom) -- noqa: AL03
-FROM received.neighborhood_ways_intersections AS ints,
-    received.neighborhood_ways_net_vert AS vert1,
-    received.neighborhood_ways AS roads1,
-    received.neighborhood_ways_net_vert AS vert2,
-    received.neighborhood_ways AS roads2
-WHERE
-    vert1.road_id = roads1.road_id
-    AND vert2.road_id = roads2.road_id
-    AND ints.int_id = roads1.intersection_from
-    AND ints.int_id = roads2.intersection_to
-    AND roads1.one_way = 'tf'
-    AND roads2.one_way = 'tf'
-    AND roads1.road_id != roads2.road_id;
-
--- to-from to from-to
-INSERT INTO received.neighborhood_ways_net_link (
-    int_id, source_vert, target_vert, geom
-)
-SELECT
-    ints.int_id,
-    vert1.vert_id,
-    vert2.vert_id, -- noqa: AL08
-    ST_Makeline(vert1.geom, vert2.geom) -- noqa: AL03
-FROM received.neighborhood_ways_intersections AS ints,
-    received.neighborhood_ways_net_vert AS vert1,
-    received.neighborhood_ways AS roads1,
-    received.neighborhood_ways_net_vert AS vert2,
-    received.neighborhood_ways AS roads2
-WHERE
-    vert1.road_id = roads1.road_id
-    AND vert2.road_id = roads2.road_id
-    AND ints.int_id = roads1.intersection_from
-    AND ints.int_id = roads2.intersection_from
-    AND roads1.one_way = 'tf'
-    AND roads2.one_way = 'ft'
-    AND roads1.road_id != roads2.road_id;
+    src.int_id,
+    src.vert_id,
+    tgt.vert_id, -- noqa: AL08
+    ST_Makeline(src.geom, tgt.geom) -- noqa: AL03
+FROM src
+INNER JOIN tgt
+    ON
+        src.int_id = tgt.int_id
+        AND src.road_id != tgt.road_id;
 
 -- index
 CREATE INDEX idx_neighborhood_ways_net_vert_road_id
